fd61972edb467f2a4f1cb16b863379fc1ee0af3c
Matthew Wang <mattw24@bu.edu>
2024-04-12 22:48:00 -0400

    spotify auth

--- spotify-api.py
+++ spotify-api.py
@@ -78,22 +78,81 @@ def search_for_album(token, album):
 
     return json_result[0]
 
+def search_for_song(token, song):
+
+    url = "https://api.spotify.com/v1/search"
+    query = f"?q={song}&type=track&limit=1"
+
+    query_url = url + query
+
+    headers = get_auth_header(token)
+
+    result = get(query_url, headers=headers)
+
+    json_result = json.loads(result.content)["tracks"]["items"]
+
+    if len(json_result) == 0:
+        print("No song with this name exists")
+        return None
+    
+    return json_result[0]
+
+def get_bpm_of_song(token, song_id):
+    url = f"https://api.spotify.com/v1/audio-features/{song_id}"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)
+    return json_result["tempo"]
+
+def match_songs_by_bpm(token, song_id, bpm):
+    url = "https://api.spotify.com/v1/recommendations"
+    headers = get_auth_header(token)
+    query = f"?seed_tracks={song_id}&target_tempo={bpm}&limit=10"
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
+
+def get_album_tracks(token, album_id):
+    url = f"https://api.spotify.com/v1/albums/{album_id}/tracks"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)["items"]
+    return json_result
+
+def display(songs):
+    for idx, song in enumerate(songs):
+        print(f"{idx + 1}, {song['name']}")
+
 
 # Example usage:
+if __name__ == "__main__":
 
-# get token 
-token = get_token()
+    # get token
+    token = get_token()
+    
+    # search for artist
+    artist = search_for_artist(token, "Michael Jackson")["id"]
+    print("Artist ID: ", artist)
 
-# get artist id
-artist = search_for_artist(token, "Marvin Gaye")
-artist_id = artist["id"]
+    # get songs by artist
+    #songs_by_artist = get_songs_by_artist(token, artist)
+    #print("Songs by artist: ", songs_by_artist)
 
-# get songs
-songs = get_songs_by_artist(token, artist_id)
+    # search for album
+    album = search_for_album(token, "Off the Wall")["id"]
+    print("Album ID: ", album)
 
-# Display top 10 songs for artist
-for idx, song in enumerate(songs):
-   print(f"{idx + 1}, {song['name']}")
+    # search for song
+    song = search_for_song(token, "Rock with You")["id"]
+    print("Song ID: ", song)
+
+    # get bpm of song
+    bpm = get_bpm_of_song(token, song)
+    print("bpm: ", bpm)
+
+    matching_songs = match_songs_by_bpm(token, song, bpm)
+    display(matching_songs)
     
 
 
